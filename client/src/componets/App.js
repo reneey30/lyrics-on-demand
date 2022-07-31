5170fce3553ddcb82099c6784c20ba696da8e61a
Irene Njeru <irenenjeru8@gmail.com>
2022-07-31 18:55:43 +0300

    create Signup component

--- client/src/componets/App.js
+++ client/src/componets/App.js
@@ -1,42 +1,52 @@
-
-// function App() {
-//   return (
-//     <>
-//       <h1>Jambo</h1>
-//     </>
-//   );
-// }
 import React from "react"
-import Signup from "./Signup"
+import Signup from "./Signup";
 import { Container } from "react-bootstrap"
-import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
-import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
-import UpdateProfile from "./UpdateProfile"
+
 
 function App() {
   return (
     <Container
-      className="d-flex align-items-center justify-content-center"
+      className="d-flex justify-content-center mt-4"
       style={{ minHeight: "100vh" }}
     >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-                      </Switch>
-          </AuthProvider>
-        </Router>
+      <div className="w-100" style={{ maxWidth: "500px" }}>
+        <Signup />
       </div>
+      
     </Container>
-  )
+  );
 }
+// import React from "react"
+// import Signup from "./Signup"
+// import { Container } from "react-bootstrap"
+// import { AuthProvider } from "../contexts/AuthContext"
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// import Dashboard from "./Dashboard"
+// import Login from "./Login"
+// import PrivateRoute from "./PrivateRoute"
+// import UpdateProfile from "./UpdateProfile"
+
+// function App() {
+//   return (
+//     <Container
+//       className="d-flex align-items-center justify-content-center"
+//       style={{ minHeight: "100vh" }}
+//     >
+//       <div className="w-100" style={{ maxWidth: "400px" }}>
+//         <Router>
+//           <AuthProvider>
+//             <Switch>
+//               <PrivateRoute exact path="/" component={Dashboard} />
+//               <PrivateRoute path="/update-profile" component={UpdateProfile} />
+//               <Route path="/signup" component={Signup} />
+//               <Route path="/login" component={Login} />
+//                       </Switch>
+//           </AuthProvider>
+//         </Router>
+//       </div>
+//     </Container>
+//   )
+// }
 
 
 export default App;
