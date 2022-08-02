d9c305e429a93216de9d61a52e3c77206a2fb6a9
Irene Njeru <irenenjeru8@gmail.com>
2022-08-02 11:19:18 +0300

    edit user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,25 @@
 class UsersController < ApplicationController
-    before_action :authorize, only: [:show]
-    def create
-      user = User.create(user_params)
-  
-      if user.valid?
-        session[:user_id] = user.id
-        render json: user, status: :created
-      else
-        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    def show
-      user = User.find_by(id: session[:user_id])
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  before_action :authorize, except: [:create]
+
+  def create
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+  end
+
+  def update
+      user = User.find(session[:user_id])
+      user.update!(user_params)
+      render json: user, status: :accepted
+  end
+
+  def show
+      current_user = User.find(session[:user_id])
+      render json: current_user, status: :created
+  end
   
       if user
         render json: user
