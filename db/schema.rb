588b713f0aa95d2c9845c0a913076f7578554365
Irene Njeru <irenenjeru8@gmail.com>
2022-08-06 16:25:26 +0300

    create user and favourite in db and migrated them - migrate down to clear error

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_06_125427) do
+ActiveRecord::Schema.define(version: 2022_08_03_163254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "favourites", force: :cascade do |t|
-    t.string "artist"
-    t.string "title"
-    t.text "lyrics"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "user_email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
